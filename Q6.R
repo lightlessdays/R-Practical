a9cd448c9783661d2d3a53721b42bf8af5abe793
Dhruv Badaya <dhruvbadaya@gmail.com>
2023-05-06 14:56:46 +0530

    Create Q6.R

---
+++ Q6.R
@@ -0,0 +1,9 @@
+# A
+set.seed(123) # for reproducibility
+sim1 <- rbinom(n = 1, size = 100, prob = 0.5)
+sim2 <- rbinom(n = 1, size = 100, prob = 0.5)
+
+# B
+set.seed(123) # for reproducibility
+sim1 <- rnorm(n = 10, mean = 10, sd = 10)
+sim2 <- rnorm(n = 10, mean = 100, sd = 100)
